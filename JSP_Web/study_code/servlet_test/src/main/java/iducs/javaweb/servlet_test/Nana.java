fc33b5051d60822e9013719d77df989cefa1c6c6
woogie <jeonguk29@naver.com>
2022-10-08 22:25:22 +0900

    인텔리제이를 이용한 JSP 웹프로그래밍 환경구축

--- JSP_Web/study_code/servlet_test/src/main/java/iducs/javaweb/servlet_test/Nana.java
+++ JSP_Web/study_code/servlet_test/src/main/java/iducs/javaweb/servlet_test/Nana.java
@@ -1,2 +1,22 @@
-package iducs.javaweb.servlet_test;public class Nana {
+package iducs.javaweb.servlet_test;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+@WebServlet(name = "NanaServlet", value = "/Nana-servlet")
+public class Nana extends HttpServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        //ServletRequest req : 서블릿을 만들때 필요한 요청 도구, ServletResponse res : 출력 도구
+        //super.service(req, res);
+       // OutputStream os = req.getOutputSteam();
+       // printStream out = new PrintStream(os, true);
+       // out.println("Hello Servlet!!");
+    }
 }
